6bd5648b71aca4b5e68b2c3100951ddadc3ad6ec
Friar-Bread <143951470+Friar-Bread@users.noreply.github.com>
2025-12-30 23:03:17 -0500

    Changed logic and added functions

--- car_sorter.py
+++ car_sorter.py
@@ -2,6 +2,11 @@ import json #For saving JSON data on each car
 from pathlib import Path #For saving to folders
 from datetime import date #For getting current date
 
+#function to print a list over multiple lines
+def print_list(list_to_print):
+    for i in list_to_print:
+        print(i)
+
 #Comparing current year with model year to get age
 def years_since (year_in):
     now = date.today()
@@ -62,24 +67,6 @@ def input_then_cap(question):
     answer = answer.capitalize()
     return (answer)
 
-#Getting data for a new car
-def new_car_input():
-    car_make = input_then_cap("What is the make of the car? ")
-    car_model = input_then_cap("What is the model of the car? ")
-    car_year = int(input_then_cap("What is the year of the car? "))
-    is_car_fav = False
-    car_rating = int(input_then_cap("Give your rating of the car 1-5 "))
-    car_note = "No note yet..."
-    car_name = str(car_year)+car_make+car_model
-    car_dict = dict(name = car_name, make = car_make, model = car_model, year = car_year, favorite = is_car_fav, rating = car_rating, notes = car_note)
-    return car_dict
-
-#Getting car data and storing it's files
-def new_car():
-    car = new_car_input()
-    add_to_saved_cars(car['name'])
-    save_car_to_json(car, car['name'])
-
 #Check if a car is saved and return its proper name
 def check_if_car(car):
     inspect_car_lower = car.lower()
@@ -91,10 +78,39 @@ def check_if_car(car):
             car_to_inspect = car
     return car_to_inspect
 
+def get_car_name_input(message):
+    user_in = input(str(message))
+    car_name = check_if_car(user_in)
+    return car_name
+
+data_needing_str = ["make","model","note"]
+data_needing_int = ["year","rating"]
+data_needing_bool = ["favorite"]
+data_questions = dict(make = "What is the make of the car? ",
+                      model = "What is the model of the car? ",
+                      year = "What is the year of the car? ",
+                      rating = "Give your rating of the car 1-5 "
+                      )
+#Getting data for a new car
+def new_car_input():
+    car_make = input_then_cap(data_questions["make"])
+    car_model = input_then_cap(data_questions["model"])
+    car_year = int(input_then_cap(data_questions["year"]))
+    is_car_fav = False
+    car_rating = int(input_then_cap(data_questions["rating"]))
+    car_note = "No note yet..."
+    car_name = str(car_year)+car_make+car_model
+    car_dict = dict(name = car_name, make = car_make, model = car_model, year = car_year, favorite = is_car_fav, rating = car_rating, note = car_note)
+    return car_dict
+
+#Getting car data and storing it's files
+def new_car():
+    car = new_car_input()
+    add_to_saved_cars(car['name'])
+    save_car_to_json(car, car['name'])
+
 #Function to inspect the data about a car
-def inspect_car(car):
-    print("\n")
-    car_to_inspect = check_if_car(car)
+def inspect_car(car_to_inspect):
     if car_to_inspect:
         file_path = turn_to_direct("cars",car_to_inspect)
         with open(file_path, 'r') as file:
@@ -102,25 +118,60 @@ def inspect_car(car):
         for i in car_dict:
             print(i,": ",car_dict[i])
 
-#MAIN LOOP
-while True:
+#Function for user to inspect car data
+def user_inspect_car():
+    print("\n")
+    car_to_inspect = get_car_name_input("What is the name of the car you want to look at? ")
+    inspect_car(car_to_inspect)
+
+#Function to make a dictionary of all of the car dictionaries
+def make_dictionary_of_cars():
+    cars_list = get_saved_cars()
+    cars_dict = dict()
+    for car_name in cars_list:
+        file_path = turn_to_direct("cars",car_name)
+        with open(file_path, 'r') as file:
+            car_dict = json.load(file)
+        cars_dict[car_name] = car_dict
+    return cars_dict
+
+#Edit a piece of data for a car
+def edit_car_data_point(car_name, data, new_data):
+    cars_dict = make_dictionary_of_cars()
+    print(cars_dict)
+    print(car_name)
+    car_dict = cars_dict[str(car_name)]
+    car_dict[data] = new_data
+    return car_dict
+
+#User edits a piece of data for a car
+def user_edit_car_data_point(car_name, data):
+    new_data = ""
+    question = data_questions[data]
+    if data in data_needing_str:
+        new_data = input_then_cap(question)
+    elif data in data_needing_int:
+        new_data = int(input_then_cap(question))
+    elif data in data_needing_bool:
+        new_data = True
+    if new_data:
+        car = edit_car_data_point(car_name,data, new_data)
+        add_to_saved_cars(car['name'])
+        save_car_to_json(car, car['name'])
+
+def edit_car():
+    print("")
+    car_name = get_car_name_input("What is the name of the car you want to edit? ")
+    print("")
+    inspect_car(car_name)
+    print("")
     print(
-        "\n"
-        "Hello\n"
-        "Enter N to add a car\n"
-        "Enter V to view all cars added\n"
-        "Enter I to look at a car\n"
-        "Enter E to edit a car's file\n"
-        "Enter S to search the saved cars\n"
+        "1 - Make\n"
+        "2 - Model\n"
+        "3 - Year\n"
+        "4 - Rating"
     )
-    plan = input_then_cap("What would you like to do?")
-    print ("\n")
-    if plan == "N":
-        new_car()
-    elif plan == "V":
-        for i in get_saved_cars():
-            print (i)
-    elif plan == "I":
-        inspect_car(input("What is the name of the car you want to look at "))
-    elif plan == "E":
-        pass
+    data_input = input("What piece of data do you want to edit? ")
+    things_possible_to_edit = ['make','model','year','rating']
+    chosen_data = things_possible_to_edit[int(data_input)-1]
+    user_edit_car_data_point(car_name, chosen_data)
---
+++ main_loop.py
@@ -0,0 +1,26 @@
+from car_sorter import input_then_cap, new_car, get_saved_cars, user_inspect_car, print_list, edit_car
+
+#MAIN LOOP
+while True:
+    print(
+        "\n"
+        "Hello\n"
+        "Enter N to add a car\n"
+        "Enter V to view all cars added\n"
+        "Enter I to look at a car\n"
+        "Enter E to edit a car's file\n"
+        "Enter S to search the saved cars\n"
+    )
+    plan = input_then_cap("What would you like to do? ")
+    print ("\n")
+
+    if plan == "N":
+        new_car()
+    elif plan == "V":
+        print_list(get_saved_cars())
+    elif plan == "I": 
+        user_inspect_car()
+    elif plan == "E":
+        edit_car()
+    elif plan == "S":
+        pass
